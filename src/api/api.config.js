69e88035523052ec4e30d87ecfea9ec6acbfabb7
Vitalii <74697842+VitaliiKutsenko@users.noreply.github.com>
2023-02-20 08:53:43 +0300

    Update api.config.js

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const API_URL = `https://cv-server-taupe.vercel.app/api`;
-export const API_URL = `http://localhost:5000/api`;
+export const API_URL = `https://cv-server-taupe.vercel.app/api`;
+// export const API_URL = `http://localhost:5000/api`;
 
 export const $api = axios.create({
   baseURL: API_URL,
